122c32cfd845bb7959418d2e2d8d45773a629f8f
isimmons <isimmons33@gmail.com>
2024-03-22 23:46:12 -0400

    node adapter ?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import db from "@astrojs/db";
 import { defineConfig } from "astro/config";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [db()],
   site: "https://isimmons.github.io",
+  adapter: node({
+    mode: "standalone"
+  })
 });
